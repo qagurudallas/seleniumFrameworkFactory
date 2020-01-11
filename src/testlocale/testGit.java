45cfc9607ee5c3b029ad35820555b302d9fc237c
Arunaa2sam <arunaa2sam@gmail.com>
2020-01-11 15:29:11 -0600

    aruna

--- src/testlocale/testGit.java
+++ src/testlocale/testGit.java
@@ -3,10 +3,11 @@ package testlocale;
 public class testGit {
 
 	//test qa guru
-<<<<<<< HEAD
+//<<<<<<< HEAD
 	//test vijaya
-=======
+//=======
 	// test gowri
 	//test tamil
->>>>>>> b71247a83c6f9e4c4f4d4ef2d9c7675cd6d4bc65
+//test aruna
+//>>>>>>> b71247a83c6f9e4c4f4d4ef2d9c7675cd6d4bc65
 }
